7d91b3b81e1c974202d82a93c28df383cf60ee3b
nick-dev12 <webgeniuses12@gmail.com>
2023-09-30 11:28:47 +0200

    valider

--- index.php
+++ index.php
@@ -269,6 +269,9 @@ session_start();
 
 
     <?php include('footer.php') ?>
+
+
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/Flip.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
